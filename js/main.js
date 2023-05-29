60ec2234372077d207b6c90e3514c9e327a0b5c8
Karimullah <59876062+KarimullahOfficial@users.noreply.github.com>
2023-05-29 11:36:43 +0500

    p

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
+// let nav = document.querySelector(".navbar");
+// window.onscroll = function () {
+//   if (document.documentElement.scrollTop > 100) {
+//     nav.classList.add("header-scrolled");
+//   } else {
+//     nav.classList.remove("header-scrolled");
+//   }
+// };
 let nav = document.querySelector(".navbar");
 window.onscroll = function () {
+  console.log("Scroll event triggered!"); // Add this line to check if the code is executing
   if (document.documentElement.scrollTop > 100) {
     nav.classList.add("header-scrolled");
   } else {
-    nav.classList.remove("header-scrollessssssd");
+    nav.classList.remove("header-scrolled");
   }
 };
-let navBar = document.querySelectorAll(".nav-link");
-let navCollapse = document.querySelectorAll(".navbar-collapse .collapse");
-navBar.forEach(function (a) {
-  a.addEventListener("click", function () {
-    navCollapse.classList.remove("show");
-  });
-});
+
+// const navBar = document.querySelectorAll(".nav-link");
+// const navCollapse = document.querySelectorAll(".navbar-collapse  ");
+// navBar.forEach(function (a) {
+//   a.addEventListener("click", function () {
+//     console.log("Scroll event triggered!");
+//     navCollapse.classList.add("hide");
+//   });
+// });
